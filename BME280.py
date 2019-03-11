555185fa02c4a4daf9dbc0ad42061c90e18c9c86
Sahil Ashar <sahilashar16@gmail.com>
2019-03-11 17:37:40 -0500

    todo's for Dylan

---
+++ BME280.py
@@ -0,0 +1,9 @@
+# TODO: @Dylan
+#
+# Set-Up BME280.py
+# Don't worry about library downloads
+# Instead of printing data like the GitHub repo shows,
+# need to be stored in a text file called bme280.txt
+#
+# Look up how to do that in Python
+# Look over all documentation for all sensors(in GitHub repo)
