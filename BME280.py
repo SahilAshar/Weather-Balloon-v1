824ac612a2f8b386bdb3b4c1d56c7333e25aeab4
Dylan Tabarini <dtabarini@gmail.com>
2019-03-11 19:32:13 -0500

    Adding BME280 and bno055, completed not tested with sensors

--- BME280.py
+++ BME280.py
@@ -7,3 +7,31 @@
 #
 # Look up how to do that in Python
 # Look over all documentation for all sensors(in GitHub repo)
+
+#let use this to create text file in initilization
+
+
+import datetime
+import time
+import board
+import busio
+import adafruit_bme280
+
+f = open("bme280.txt", "a+") #appends a file, creates new one if not there, can create file in the initilization by f = open("bme280.txt", "w+")
+f.write("time    tempurature    humidity    pressure    altitude\n")
+
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+# change this to match the location's pressure (hPa) at sea level
+bme280.sea_level_pressure = 1013.25
+
+while True:
+    f.write(str(datetime.datetime.now()) )
+    f.write("    %0.1f" % bme280.temperature)
+    f.write("    %0.1f" % bme280.humidity)
+    f.write("    %0.1f" % bme280.pressure)
+    f.write("    %0.2f\n" % bme280.altitude)
+    time.sleep(2) #however long we want, in seconds
+
+
---
+++ bno055.py
@@ -0,0 +1,25 @@
+
+import datetime
+import time
+#import board
+#import busio
+#import adafruit_bno055
+
+f = open("bno055.txt", "a+") #appends a file, creates new one if not there, can create file in the initilization by f = open("bme280.txt", "w+")
+f.write("time    Temperature    Accelerometer    Magnetometer    Gyroscope    Euler angle     Quaternion    Linear acceleration    Gravity\n")
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055(i2c)
+
+
+while True:
+
+    f.write('{}    '.format(sensor.temperature))
+    f.write('{}    ' .format(sensor.accelerometer))
+    f.write('{}    '.format(sensor.magnetometer))
+    f.write('{}    '.format(sensor.gyroscope))
+    f.write('{}    '.format(sensor.euler))
+    f.write('{}    '.format(sensor.quaternion))
+    f.write('{}    '.format(sensor.linear_acceleration))
+    f.write('{}\n'.format(sensor.gravity))
+    time.sleep(1) #however long we want, in seconds
+
